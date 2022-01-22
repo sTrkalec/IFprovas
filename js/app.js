917f02d8de357ba8eed5bc39aa8fecd038dec571
oZo <vytorgabriel123v@gmail.com>
2022-01-21 23:35:12 -0300

    Create news pages

--- js/app.js
+++ js/app.js
@@ -5,8 +5,9 @@ var btnSignup = document.querySelector("#signup");
 var body = document.querySelector("body");
 
 
-btnSignin.addEventListener("click", function () {
+btnSignin.addEventListener("click",  function () {
    body.className = "sign-in-js"; 
+
 });
 
 btnSignup.addEventListener("click", function () {
